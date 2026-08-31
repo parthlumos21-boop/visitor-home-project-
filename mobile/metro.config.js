147af9b196240991669c6cedd0b825a393912a9b
Your Name <parthlumos21@gmail.com>
2026-08-31 16:22:49 +0530

    Optimize EAS Build size and memory

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -3,4 +3,14 @@ const { withNativeWind } = require("nativewind/metro");
 
 const config = getDefaultConfig(__dirname);
 
+// Ignore the backend and frontend directories to prevent Metro from scanning them and running out of memory
+config.resolver.blockList = [
+  /.*\/backend\/.*/,
+  /.*\/frontend\/.*/,
+];
+
+// Only watch the mobile directory and the root node_modules
+const path = require('path');
+config.watchFolders = [__dirname, path.resolve(__dirname, '../node_modules')];
+
 module.exports = withNativeWind(config, { input: "./global.css" });
